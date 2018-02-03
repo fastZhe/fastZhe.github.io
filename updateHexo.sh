1a726778f599fca1b9cd11d021692d9c13a7d80c
huangzhe <526910607@qq.com>
2018-02-03 22:18:53 +0800

    commit new msg:20180203Feb18

---
+++ updateHexo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+curDate=$(date +%Y%m%d%h%M)
+defaultmsg="commit new msg:${curDate}"
+if [ ! -z "$1" ]; then
+defaultmsg=$1
+fi
+
+echo "use msg:${defaultmsg}"
+
+basedir=$(cd `dirname $0`;pwd)
+echo $basedir
+
+cd $basedir
+#生成html
+hexo g
+#git add .
+git add .
+#commit 
+git commit -m "${defaultmsg}"
+#git push hexo
+git push origin hexo
+#hexo push master
+hexo d
+
+echo "everything is done"
